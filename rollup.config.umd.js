2c9aad9097065ee30d37273d14f3c1ed0a3c4e47
Baldur Bjarnason <bb@baldurbjarnason.com>
2016-02-16 17:25:22 +0000

    Uglified UMD output

--- rollup.config.umd.js
+++ rollup.config.umd.js
@@ -4,6 +4,6 @@ import uglify from 'rollup-plugin-uglify';
 config.format = 'umd';
 config.dest = 'dist/index.umd.js';
 config.moduleName = 'rollupStarterProject';
-// config.plugins.push(uglify());
+config.plugins.push(uglify());
 
 export default config;
